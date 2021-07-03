c244640ce1d6d77e4ba647e37f53d114f0a68bb3
FallenAngel <1544931543qq.com>
2021-07-04 00:15:21 +0800

    README.md 更新
    使用组件重构静态“Todo List”页面完成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, {Component} from "react";
 
-import Hello from "./Hello";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+import "./App.css"
 
 export default class App extends Component {
     render() {
-        return <Hello/>
+        return (
+            <div className="todo-container">
+                <div className="todo-wrap">
+                    <Header/>
+                    <Main/>
+                    <Footer/>
+                </div>
+            </div>
+        )
     }
 }
--- src/Hello/index.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-import "./index.css"
-
-export default class Hello extends Component {
-    render() {
-        return <h2>测试文本</h2>
-    }
-}
